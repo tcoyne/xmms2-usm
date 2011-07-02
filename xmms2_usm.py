0be99a81cab20656336be53b18e1a83c99cb4cb9
Thomas Coyne <t.tcoyne@gmail.com>
2011-07-02 12:13:41 -0600

    Add mpris Playlists support

--- xmms2_usm.py
+++ xmms2_usm.py
@@ -21,16 +21,34 @@ from glib import timeout_add_seconds
 import os
 import sys
 import gobject
+import dbus
+import dbus.service
+from sets import Set
 
 #from sound_menu import SoundMenuControls
 
 from dbus.mainloop.glib import DBusGMainLoop
+import dbus.mainloop.glib
+
+class XMMS2Playlist(dbus.service.Object):
+
+    def __init__(self, name, description = ""):
+
+        self.name = name
+        self.description = description
+        self.path = "/org/mpris/MediaPlayer2/xmms2/playlists/%s" % name
+        dbus.service.Object.__init__(self, dbus.SessionBus(), self.path)
+
+    def getPlaylist(self):
+        return (self.path, self.name, self.description)
+
 
 class XMMS2USM:
 
-
     def __init__(self):
-        self.ml = gobject.MainLoop(None, False)
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.ml = gobject.MainLoop()
+        #self.ml = gobject.MainLoop(None, False)
 
         self.xmms = xmmsclient.XMMS("ubuntu-sound-menu")
         try:
@@ -45,6 +63,8 @@ class XMMS2USM:
         """self.xmms.playback_current_id(self.my_current_id)"""
         self.xmms.broadcast_playback_status(self.update_playback_status)
         self.xmms.broadcast_playback_current_id(self.update_nowplaying)
+        self.xmms.broadcast_playlist_changed(self.signal_update_playlists)
+        self.xmms.broadcast_playlist_loaded(self.update_active_playlist)
         self.sound_menu = SoundMenuControls('xmms2')
         self.sound_menu._sound_menu_next = self._sound_menu_next
         self.sound_menu._sound_menu_previous = self._sound_menu_previous
@@ -52,14 +72,21 @@ class XMMS2USM:
         self.sound_menu._sound_menu_play = self._sound_menu_play
         self.sound_menu._sound_menu_pause = self._sound_menu_pause
         self.sound_menu._sound_menu_raise = self._sound_menu_raise
+        self.sound_menu._get_playlists = self._get_playlists
+        self.sound_menu._active_playlist = self._active_playlist
+        self.sound_menu._activate_playlist = self._activate_playlist
 
         self.playback_status = False
+        self.playlists = []
+        self.current_playlist = ""
         timeout_add_seconds(1, self.firstupdate)
         self.ml.run()
 
     def firstupdate(self):
         self.xmms.playback_status(self.update_playback_status)
         self.xmms.playback_current_id(self.update_nowplaying)
+        self.xmms.playlist_current_active(self.update_active_playlist)
+        self.xmms.playlist_list(self.update_playlists)
 
     def dieme(self, etc):
         self.ml.quit()
@@ -99,6 +126,75 @@ class XMMS2USM:
         else:
             self.playback_status = False
             self.sound_menu.signal_paused()
+
+    def _get_playlists(self, index=None, maxCount=None, order=None, reverseOrder=None):
+
+        if index is None:
+            index = 0
+        if maxCount is None:
+            maxCount = len(self.playlists)
+
+        playlists = []
+        for playlist in self.playlists[index:(maxCount + index)]:
+            playlists.append(playlist.getPlaylist())
+
+        if len(playlists) == 0:
+            raise Exception("""No playlists available yet""")
+
+        return playlists
+
+    def signal_update_playlists(self, result = None):
+        self.update_playlists()
+
+    def update_active_playlist(self, result = None):
+        if result == None:
+            self.xmms.playlist_current_active(self.update_active_playlist)
+        else:
+            self.active_playlist = (True, (result.value(), result.value(), ""))
+            self.sound_menu._signal_active_playlist()
+
+    def _active_playlist(self):
+        return self.active_playlist
+
+    def update_playlists(self, result = None):
+        if result == None:
+            self.xmms.playlist_list(self.update_playlists)
+        else:
+            current = Set()
+            old = Set()
+
+            for list in result.value():
+                if list != "_active":
+                    current.add(list)
+
+            for playlist in self.playlists:
+                old.add(playlist.name)
+
+            for oldList in (old-current):
+                for list in self.playlists:
+                    if(list.name == oldList):
+                        self.playlists.remove(list)
+
+            for newList in (current-old):
+                self.playlists.append(XMMS2Playlist(newList, ""))
+
+            self.sound_menu._signal_playlist_count()
+
+
+    def activatePlaylistCallback(self, result = None):
+
+        if result != None:
+            print result.iserror()
+            print result.value()
+        else:
+            print "Result is none"
+
+    def _activate_playlist(self, playlistpath):
+        split = playlistpath.rsplit('/', 2)
+        playlist = split[2]
+        result = self.xmms.playlist_load(playlist, self.activatePlaylistCallback)
+        self.xmms.playlist_current_active(self.update_active_playlist)
+        self.xmms.playback_current_id(self.update_nowplaying)
 
     def update_nowplaying(self, result):
         v = result.value()
@@ -237,9 +333,6 @@ _sound_menu_pause
 
 """
 
-import dbus
-import dbus.service
-
 class SoundMenuControls(dbus.service.Object):
     """
     SoundMenuControls - A class to make it easy to integrate with the Ubuntu Sound Menu.
@@ -261,7 +354,7 @@ class SoundMenuControls(dbus.service.Object):
 
         self.desktop_name = desktop_name
         bus_str = """org.mpris.MediaPlayer2.%s""" % desktop_name
-        bus_name = dbus.service.BusName(bus_str, bus=dbus.SessionBus())
+        bus_name = dbus.service.BusName(name=bus_str, bus=dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, "/org/mpris/MediaPlayer2")
         self.__playback_status = "Stopped"
 
@@ -361,7 +454,7 @@ class SoundMenuControls(dbus.service.Object):
 
         """
 
-        return [DesktopEntry, PlaybackStatus, MetaData]
+        return dict({"PlaylistCount":self.PlaylistCount(), "ActivePlaylist":self.ActivePlaylist, "DesktopEntry":self.DesktopEntry, "PlaybackStatus":self.PlaybackStatus, "MetaData":self.MetaData})
 
     @property
     def DesktopEntry(self):
@@ -584,7 +677,101 @@ class SoundMenuControls(dbus.service.Object):
 
         pass
 
+    @dbus.service.method('org.mpris.MediaPlayer2.Playlists',
+                         in_signature='s', out_signature='')
+    def ActivatePlaylist(self, playlist_id):
+        """ ActivatePlaylist
+
+        A dbus signal handler to activate a playlist
+        """
+
+        self._activate_playlist(playlist_id)
+
+    def _activate_playlist(self, playlist):
+        """ _activate_playlist -
+
+        Override this function to begin playing the selected playlist
+        """
+
+        raise NotImplementedError("""@dbus.service.method('org.mpris.MediaPlayer2.Playlists')
+                                     ActivatePlaylist is not implemented by this player.""")
+
+    @dbus.service.method('org.mpris.MediaPlayer2.Playlists',
+                         in_signature='uusb', out_signature='a(sss)')
+    def GetPlaylists(self, index, maxCount, order, reverseOrder):
+        """GetPlaylists
+
+        A dbus signal handler to supply playlist information
+        """
+
+        playlists = self._get_playlists(index, maxCount, order, reverseOrder)
+        return playlists
+
+    def _get_playlists(self, index, maxCount, order, reverseOrder):
+        """ _get_playlists-
+
+        Override this function to provide a list of available playlists
+        """
+
+        raise NotImplementedError("""@dbus.service.method('org.mpris.MediaPlayer2.Playlists')
+                                     GetPlaylists is not implemented by this player.""")
+
+    @property
+    def PlaylistCount(self):
+        """PlaylistCount
+
+        The number of playlists available
+
+        This property is only used by the Sound Menu, and should not
+        be overriden or called directly.
+
+        """
+
+        return self.playlist_count
+
+    @property
+    def Orderings(self):
+        """Orderings
+
+        The ordering for the list of playlists
+
+        This property is only used by the Sound Menu, and should not
+        be overriden or called directly.
+
+        """
+
+        self.order = ["User"]
+        return self.order
+
+    @property
+    def ActivePlaylist(self):
+        """ActivePlaylist
+
+        The currently active playlist
+
+        This property is only used by the Sound Menu, and should not
+        be overriden or called directly.
+
+        """
+
+        return self._active_playlist()
 
 
+    def _active_playlist():
+        """
+        Override to provide list active playlist
+        """
+        return (False, ("/", "", ""))
+
+    def _signal_active_playlist(self):
+        d = dbus.Dictionary({"ActivePlaylist":self._active_playlist()},
+                                    "sv",variant_level=1)
+        self.PropertiesChanged("org.mpris.MediaPlayer2.Playlists",d,[])
+
+    def _signal_playlist_count(self):
+        d = dbus.Dictionary({"PlaylistCount":len(self._get_playlists())},
+                                    "sv",variant_level=1)
+        self.PropertiesChanged("org.mpris.MediaPlayer2.Playlists",d,[])
 
 x = XMMS2USM()
+
